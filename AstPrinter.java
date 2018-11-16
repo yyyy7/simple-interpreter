eb2688947f2eb6926c98f94b0a02100e3b714d70
yuqi <yuqi@atyun.net>
2018-11-16 18:23:40 +0800

    init

---
+++ AstPrinter.java
@@ -0,0 +1,49 @@
+package lox;
+
+/**
+ * AstPrinter
+ */
+public class AstPrinter implements Expr.Vistor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parentthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parentthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parentthesize(String name, Expr...exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    public static void main(String...strings) {
+        System.out.println("hello world");
+    }
+    
+}
