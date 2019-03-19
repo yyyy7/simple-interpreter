f118f7a171c067dd45fde9dd78646aa54c897310
yuqi <yuqi@atyun.net>
2019-03-19 17:31:38 +0800

    support comma operator

--- Parser.java
+++ Parser.java
@@ -8,6 +8,68 @@ import java.util.Arrays;
 
 import static lox.TokenType.*;
 
+/*
+ * Syntax Grammar
+
+ * program        → declaration* EOF ;
+
+ * declaration    → classDecl
+ *                | funDecl
+ *                | varDecl
+ *                | statement ;
+
+ * classDecl      → "class" IDENTIFIER ( "<" IDENTIFIER )?
+ *                  "{" function* "}" ;
+ * funDecl        → "fun" function ;
+ * varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
+
+ * statement      → exprStmt
+ *                | forStmt
+ *                | ifStmt
+ *                | printStmt
+ *                | returnStmt
+ *                | whileStmt
+ *                | block ;
+ * 
+ * exprStmt       → expression ";" ;
+ * forStmt        → "for" "(" ( varDecl | exprStmt | ";" )
+ *                            expression? ";"
+ *                            expression? ")" statement ;
+ * ifStmt         → "if" "(" expression ")" statement ( "else" statement )? ;
+ * printStmt      → "print" expression ";" ;
+ * returnStmt     → "return" expression? ";" ;
+ * whileStmt      → "while" "(" expression ")" statement ;
+ * block          → "{" declaration* "}" ;
+
+ * expression     → assignment ( "," assignment)* ;
+ * 
+ * assignment     → ( call "." )? IDENTIFIER "=" assignment
+ *                | logic_or;
+ * 
+ * logic_or       → logic_and ( "or" logic_and )* ;
+ * logic_and      → equality ( "and" equality )* ;
+ * equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+ * comparison     → addition ( ( ">" | ">=" | "<" | "<=" ) addition )* ;
+ * addition       → multiplication ( ( "-" | "+" ) multiplication )* ;
+ * multiplication → unary ( ( "/" | "*" ) unary )* ;
+ * 
+ * unary          → ( "!" | "-" ) unary | call ;
+ * call           → primary ( "(" arguments? ")" | "." IDENTIFIER )* ;
+ * primary        → "true" | "false" | "nil" | "this"
+ *                | NUMBER | STRING | IDENTIFIER | "(" expression ")"
+ *                | "super" "." IDENTIFIER ;
+
+ * function       → IDENTIFIER "(" parameters? ")" block ;
+ * parameters     → IDENTIFIER ( "," IDENTIFIER )* ;
+ * arguments      → expression ( "," expression )* ;
+
+ * NUMBER         → DIGIT+ ( "." DIGIT+ )? ;
+ * STRING         → '"' <any char except '"'>* '"' ;
+ * IDENTIFIER     → ALPHA ( ALPHA | DIGIT )* ;
+ * ALPHA          → 'a' ... 'z' | 'A' ... 'Z' | '_' ;
+ * DIGIT          → '0' ... '9' ;
+*/
+
 /**
  * Parser
  */
@@ -201,7 +263,13 @@ public class Parser {
     }
     
     private Expr expression() {
-        return assignment();
+        Expr expr = assignment();
+        // List<Expr> commaExprs = new ArrayList<>();
+        while (match(COMMA)) { 
+            expr = assignment();
+            // commaExprs.add(expr);
+        }
+        return expr;
     }
 
     private Expr assignment() {
